bd2882f15ad715742c24f28599813e336f6e6ffd
Jardel-S <jardelmoreno29@gmail.com>
2023-09-19 12:05:42 +0100

    First Model-Controller-View - Departement

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // GET: Departments
+        public ActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Appliances" });
+
+            return View(list);
+        }
+    }
+}
